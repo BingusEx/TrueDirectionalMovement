7a037e37eaa2e0bd0901bb8bee3a3333a7ab587c
BingusEx <10480438+BingusEx@users.noreply.github.com>
2024-09-10 13:18:04 +0200

    Add some error checking/handling to the GTS functions

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -262,8 +262,8 @@ RE::hkVector4 GetBoneQuad(const RE::Actor* a_actor, const char* a_boneStr, const
 	}
 	return NiPointToHkVector(Bone->local.translate, true);
 }
-RE::NiAVObject* FindBoneNode(const RE::Actor* a_actorptr, const std::string_view a_nodeName, const bool a_isFirstPerson)
-{
+
+RE::NiAVObject* FindBoneNode(const RE::Actor* a_actorptr, const std::string_view a_nodeName, const bool a_isFirstPerson) {
 	if (!a_actorptr->Is3DLoaded())
 		return nullptr;
 
@@ -278,14 +278,26 @@ RE::NiAVObject* FindBoneNode(const RE::Actor* a_actorptr, const std::string_view
 	// Game lookup failed we try and find it manually
 	std::deque<RE::NiAVObject*> queue;
 	queue.push_back(model);
-
+	int RecursionCheck = 512;
+	//Normally this should not happen. And testing has proven this, lets just keep it in for safety.
 	while (!queue.empty()) {
 		const auto currentnode = queue.front();
 		queue.pop_front();
+		int RecursionCheckFor = 512;
+		if (RecursionCheck-- <= 0) {
+			//MessageBoxA(NULL, "RECURSION BUG IN FindBoneNode()", "TrueDirectionalMovement", 0);
+			queue.clear();
+			return nullptr;
+		}
 		try {
 			if (currentnode) {
 				if (const auto ninode = currentnode->AsNode()) {
 					for (const auto& child : ninode->GetChildren()) {
+						if (RecursionCheckFor-- <= 0) {
+							//MessageBoxA(NULL, "RECURSION BUG IN FindBoneNode() For Loop", "TrueDirectionalMovement", 0);
+							queue.clear();
+							return nullptr;
+						}
 						// Bredth first search
 						queue.push_back(child.get());
 						// Depth first search
@@ -297,17 +309,26 @@ RE::NiAVObject* FindBoneNode(const RE::Actor* a_actorptr, const std::string_view
 					return currentnode;
 				}
 			}
-		} catch (const std::overflow_error& e) {
+		}
+		catch (const std::overflow_error& e) {
 			SKSE::log::warn("Find Bone Overflow: {}", e.what());
+			//MessageBoxA(NULL, "Overflow Exception in FindBoneNode()", "TrueDirectionalMovement", 0);
+			return nullptr;
 		}  // this executes if f() throws std::overflow_error (same type rule)
 		catch (const std::runtime_error& e) {
 			SKSE::log::warn("Find Bone Underflow: {}", e.what());
+			//MessageBoxA(NULL, "Underflow Exception in FindBoneNode()", "TrueDirectionalMovement", 0);
+			return nullptr;
 		}  // this executes if f() throws std::underflow_error (base class rule)
 		catch (const std::exception& e) {
 			SKSE::log::warn("Find Bone Exception: {}", e.what());
+			//MessageBoxA(NULL, "STD Exception in FindBoneNode()", "TrueDirectionalMovement", 0);
+			return nullptr;
 		}  // this executes if f() throws std::logic_error (base class rule)
 		catch (...) {
 			SKSE::log::warn("Find Bone Exception Other");
+			//MessageBoxA(NULL, "Other Exception in FindBoneNode()", "TrueDirectionalMovement", 0);
+			return nullptr;
 		}
 	}
 
